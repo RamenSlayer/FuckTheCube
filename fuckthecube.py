b968e00d2f1fc0ec3f80e6da4c6f0350e9983d60
RamenSlayer <d1z.m1k3@gmail.com>
2021-02-09 10:27:01 +0300

    Now actually works

--- fuckthecube.py
+++ fuckthecube.py
@@ -9,36 +9,43 @@ bl_info = {
 }
 
 import bpy
+from bpy.types import (
+    AddonPreferences,
+    Operator,
+    Panel,
+    PropertyGroup,
+)
 
-class FuckTheCube(bpy.types.Menu):
-    bl_label = "FuckTheCube"
-    bl_idname = "cube_get_fucked"
+
+class OBJECT_OT_killonecube(Operator):
+    bl_label = "Kill the cube"
+    bl_idname = "object.cube_die"
+    bl_description = "Kills any object called cube"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_options = {'REGISTER', 'UNDO'}
     
     def execute(self, context):
+        bpy.context.active_object.select_set(False)
         try:
             bpy.data.objects['Cube'].select_set(True)
             bpy.ops.object.delete(use_global=False)
-            return {'FINISHED'}
         except:
-            return ('Oops, something went wrong')
-     
-    def draw(self, context):    
-        layout = self.layout
-        object = context.active_object
-        
-        scene = context.scene
-        col = layout.column(align=True)
+            print("there probably isn't an object named 'Cube' in the scene" 
+        return {'FINISHED'}
+
 
 
 def menu_func(self, context):
-    self.layout.operator(FuckTheCube.bl_idname)
+    self.layout.operator(OBJECT_OT_killonecube.bl_idname)
 
 def register():
-    bpy.utils.register_class(FuckTheCube)
+    bpy.utils.register_class(OBJECT_OT_killonecube)
     bpy.types.VIEW3D_MT_object.append(menu_func)
 
 def unregister():
-    bpy.utils.unregister_class(FuckTheCube)
-    
+    bpy.utils.unregister_class(OBJECT_OT_killonecube)
+    bpy.types.VIEW3D_MT_object.remove(menu_func)
+
 if __name__ == "__main__":
     register()
