81b9c5a9ca6914a967e81328c6f32d22a3acc178
RamenSlayer <d1z.m1k3@gmail.com>
2021-02-09 14:16:26 +0300

    Added notes

--- fuckthecube.py
+++ fuckthecube.py
@@ -1,3 +1,4 @@
+#just description
 bl_info = {
     "name": "Fuck The Cube",
     "author": "RamenSlayer",
@@ -8,6 +9,8 @@ bl_info = {
     "category": "Object",
 }
 
+#importing the things that might be used here, idk if they even are
+
 import bpy
 from bpy.types import (
     AddonPreferences,
@@ -16,37 +19,42 @@ from bpy.types import (
     PropertyGroup,
 )
 
+#defining the first command (delete cube by name)
 
 class OBJECT_OT_killonecube(Operator):
+#setting up options
     bl_label = "Kill the cube by name"
     bl_idname = "object.cube_die"
     bl_description = "Kills any object called cube"
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
     bl_options = {'REGISTER', 'UNDO'}
-    
+#finally the command itself
     def execute(self, context):
-        bpy.ops.object.select_all(action='DESELECT')
+        bpy.ops.object.select_all(action='DESELECT') #unselecting everything so it doesn't delete anything it shouldn't
         try:
-            bpy.data.objects['Cube'].select_set(True)
-            bpy.ops.object.delete(use_global=False)
+            bpy.data.objects['Cube'].select_set(True) #finding the object called Cube, selecting it
+            bpy.ops.object.delete(use_global=False) #deleting the object
         except:
-            print("Probably nothing named cube in the scene")
+            print("Probably nothing named cube in the scene") #people will see it only in the system console but whatever
         finally:    
-            return {'FINISHED'}
+            return {'FINISHED'} #ending the command either way
 
 
-
+#gotta define menu to add the function to ui
 def menu_func(self, context):
     self.layout.operator(OBJECT_OT_killonecube.bl_idname)
 
+#registering the command so it actually exists
 def register():
     bpy.utils.register_class(OBJECT_OT_killonecube)
     bpy.types.VIEW3D_MT_object.append(menu_func)
 
+#basically deleting it when addon disabled
 def unregister():
     bpy.utils.unregister_class(OBJECT_OT_killonecube)
     bpy.types.VIEW3D_MT_object.remove(menu_func)
 
+#idk you have to have this for the addon to actually work
 if __name__ == "__main__":
     register()
