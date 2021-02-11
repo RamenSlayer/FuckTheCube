e2a7dafe0a5368e4da2c53cb3985866c256ea359
RamenSlayer <d1z.m1k3@gmail.com>
2021-02-11 12:55:30 +0300

    Update to 0.1.5
    Now deletes multiple object with cube in the name. Can't do it if they are hidden in viewport.

--- fuckthecube.py
+++ fuckthecube.py
@@ -2,7 +2,7 @@
 bl_info = {
     "name": "Fuck The Cube",
     "author": "RamenSlayer",
-    "version": (0, 1),
+    "version": (0, 1, 5),
     "blender": (2, 80, 0),
     "location": "View3D > Object",
     "description": "Delete the default cube (if there isn't one won't do anything, must be called Cube.",
@@ -40,17 +40,36 @@ class OBJECT_OT_killonecube(Operator):
         finally:    
             return {'FINISHED'} #ending the command either way
 
+class OBJECT_OT_killallcubes(Operator):
+#setting up options again
+    bl_label = "Kill all cubes by name"
+    bl_idname = "object.cubes_die"
+    bl_description = "Kills all objects with cube in the name"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_options = {'REGISTER', 'UNDO'}
+#finally the command itself x2
+    def execute(self, context):
+        bpy.ops.object.select_all(action='DESELECT')
+        for object in bpy.data.objects:
+            if "cube" in object.name.lower():
+                object.select_set(True)
+        bpy.ops.object.delete(use_global=False)
+        return {'FINISHED'}
 
-#gotta define menu to add the function to ui
+
+#gotta define menu to add the functions to ui
 def menu_func(self, context):
     self.layout.operator(OBJECT_OT_killonecube.bl_idname)
+    self.layout.operator(OBJECT_OT_killallcubes.bl_idname)
 
-#registering the command so it actually exists
+#registering the commands so it actually exists
 def register():
     bpy.utils.register_class(OBJECT_OT_killonecube)
+    bpy.utils.register_class(OBJECT_OT_killallcubes)
     bpy.types.VIEW3D_MT_object.append(menu_func)
 
-#basically deleting it when addon disabled
+#basically deleting them when addon disabled
 def unregister():
     bpy.utils.unregister_class(OBJECT_OT_killonecube)
     bpy.types.VIEW3D_MT_object.remove(menu_func)
